9f5e7d2789e5a689659ba4be9cf8be464086e127
Ananya-Birkur <ananyabirkur97@gmail.com>
2022-10-21 22:50:46 +0530

    Worked on Base Class

--- src/test/java/com/sample/Global/BaseClass.java
+++ src/test/java/com/sample/Global/BaseClass.java
@@ -1,9 +1,49 @@
 package com.sample.Global;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseClass {
 
-	public WebDriver driver;
+	public static WebDriver driver;
+
+	public static WebDriver launchbrowser(String browsername) {
+		if (browsername.equals("chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver = new ChromeDriver();
+
+		} else if (browsername.equals("firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+		}
+		return driver;
+	}
+
+	public static void closeBrowser() {
+		driver.close();
+	}
+	
+	public static void click(By element) {
+		driver.findElement(element).click();
+	}
+	
+	public static void sendKeys(By element) {
+		driver.findElement(element).sendKeys();
+	}
+	
+	public static void select() {
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
