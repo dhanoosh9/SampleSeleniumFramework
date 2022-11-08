b82265dc476c747cb9a2fc85de386dac642388cc
dhanoosh2 <dhanoosh.donepudi@gmail.com>
2022-11-08 18:52:42 +0530

    Added maven cucumber reporting plugin

--- src/test/java/com/sample/Tests/SampleTest.java
+++ src/test/java/com/sample/Tests/SampleTest.java
@@ -4,26 +4,23 @@ import org.testng.annotations.Test;
 
 import com.sample.Global.BaseClass;
 import com.sample.PageObjects.ByObjects;
-import com.sample.PageObjects.SampleObjects;
 
 public class SampleTest extends BaseClass {
 
 	@Test(retryAnalyzer = com.sample.Utilities.RetryAnalyzer.class)
 	public void sampleTest() {
-		test = extent.createTest("sampleTest").assignAuthor("Dhanoosh").assignDevice("Dell Laptop");
-		SampleObjects sample = new SampleObjects(driver);
-		sample.login("standard_user", "secret_sauce");
+		test = extent.createTest("sampleTest").assignAuthor("Ananya").assignDevice("Hp Laptop");
+		driver.findElement(ByObjects.username).sendKeys("standard_user");
 		test.info("Entered the username");
+		driver.findElement(ByObjects.password).sendKeys("secret_sauce");
 		test.info("Entered the password");
+		driver.findElement(ByObjects.loginbtn).click();
 		test.info("Clicked on login button");
 	}
 
 	@Test
 	public void byTest() {
-//		driver.findElement(ByObjects.username).sendKeys("standard_user");
-//		driver.findElement(ByObjects.password).sendKeys("secret_sauce");
-//		driver.findElement(ByObjects.loginbtn).click();
-		test = extent.createTest("byTest").assignAuthor("Ananya").assignDevice("Hp Laptop");
+		test = extent.createTest("byTest").assignAuthor("Dhanoosh").assignDevice("Dell Laptop");
 		sendKeys(ByObjects.username, "standard_user", "Entered the username");
 		sendKeys(ByObjects.password, "secret_sauce", "Entered the password");
 		click(ByObjects.loginbtn, "Clicked on login button");
